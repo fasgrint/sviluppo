7b5715e36709cd7dacbfae7fd415b49962cb2d41
fasgrint <sessegofausto@tiscali.it>
2013-09-09 18:06:45 +0200

    first

---
+++ src/com/fasandsy/we/Page1.java
@@ -0,0 +1,17 @@
+package com.fasandsy.we;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class Page1 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.page_1);
+	}
+}
